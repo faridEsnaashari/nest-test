c5951fde3761121317bd88475d940c956943aec8
faridEsnaashari <faride13578642@gmail.com>
2020-08-12 22:38:56 +0430

    add graphql config to app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import {GraphQLModule} from '@nestjs/graphql';
+import {join} from 'path';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        GraphQLModule.forRoot({
+            typePaths: ['./**/*.graphql'],
+            definitions: {
+                path: join(process.cwd(), 'src/graphql.ts'),
+            }
+        }),
+    ],
 })
 export class AppModule {}
