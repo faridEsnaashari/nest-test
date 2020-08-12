bf87bfcfc31cf5bb711544fc3112570ab492b2fe
faridEsnaashari <faride13578642@gmail.com>
2020-08-12 22:24:34 +0430

    add sql structure

---
+++ src/sql.sql
@@ -0,0 +1,10 @@
+create database nest_test;
+\c nest_test;
+
+create table users_tbl(
+    id serial primary key,
+    name char(50),
+    age int,
+    gender char(10),
+    phonenumber char(12)
+);
