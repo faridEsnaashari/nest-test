39f741738bf20c7287701380f30409639d91ae37
faridEsnaashari <faride13578642@gmail.com>
2020-08-13 11:45:32 +0430

    add getOneUser and getAllUsers resolver for qraphql query

--- src/user/user.resolver.ts
+++ src/user/user.resolver.ts
@@ -1,4 +1,21 @@
-import { Resolver } from '@nestjs/graphql';
+import { Resolver, Query, Args } from '@nestjs/graphql';
+import {UserService} from './user.service';
 
 @Resolver('User')
-export class UserResolver {}
+export class UserResolver {
+    private readonly userServices: UserService;
+
+    constructor(){
+        this.userServices = new UserService();
+    }
+
+    @Query('user')
+    async getOneUser(@Args('user_id') user_id: number){
+        return this.userServices.getOneUser(user_id)
+    }
+
+    @Query('users')
+    async getAllUsers(){
+        return this.userServices.getAllUsers();
+    }
+}
