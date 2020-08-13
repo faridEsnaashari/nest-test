97519cb9c0e3bddbf79d5d3902b6d705ba8f1326
faridEsnaashari <faride13578642@gmail.com>
2020-08-13 13:50:57 +0430

    add UserService class and add getOneUser method

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,28 @@
 import { Injectable } from '@nestjs/common';
+import {User} from 'src/graphql';
+import {DataBaseService} from 'src/dataBase.service';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+
+    private dbManager: DataBaseService;
+    constructor(){
+        this.dbManager = new DataBaseService(); 
+    }
+
+    public async getOneUser(user_id: number): Promise<User>{
+        const queryStatement = `select * from users_tbl where id = ${ user_id }`;
+        try{
+            const result = await this.dbManager.executeQuery(queryStatement);
+            return result.rows[0];
+        }
+        catch(err){
+            console.error(err);
+        }
+    }
+
+//    getAllUsers(): User[]{
+//
+//    }
+}
+
