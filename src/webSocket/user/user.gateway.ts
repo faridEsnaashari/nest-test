13013f936597900c249dd51e3b34eaf43f392562
faridEsnaashari <faride13578642@gmail.com>
2020-08-14 21:23:08 +0430

    add getAllUsers and createUser and updateUser and deleteUser handler

--- src/webSocket/user/user.gateway.ts
+++ src/webSocket/user/user.gateway.ts
@@ -1,6 +1,6 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
 import {Socket} from 'socket.io';
-import {} from 'src/graphql';
+import {UserCreateInput, UserUpdateInput} from 'src/graphql';
 import {UserService} from './user.service';
 
 @WebSocketGateway({namespace: "websocket"})
@@ -17,5 +17,29 @@ export class UserGateway {
     async getOneUser(client: Socket, user_id: number){
         const result = await this.userServices.getOneUser(user_id);
         client.emit("getOneUser", result);
+    }
+
+    @SubscribeMessage('getAllUsers')
+    async getAllUsers(client: Socket){
+        const result = await this.userServices.getAllUsers();
+        client.emit("getAllUsers", result);
+    }
+
+    @SubscribeMessage('createUser')
+    async createUser(client: Socket, userDetails: UserCreateInput){
+        const result = await this.userServices.createUser(userDetails);
+        client.emit("createUser", result);
+    }
+
+    @SubscribeMessage('updateUser')
+    async updateUser(client: Socket, userDetails: UserUpdateInput){
+        const result = await this.userServices.updateUser(userDetails);
+        client.emit("updateUser", result);
+    }
+
+    @SubscribeMessage('deleteUser')
+    async deleteUser(client: Socket, user_id: number){
+        const result = await this.userServices.deleteUser(user_id);
+        client.emit("deleteUser", result);
     }
 }
