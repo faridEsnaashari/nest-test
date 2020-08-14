8da85554debb14499e4190f44c6eca7f4842433a
faridEsnaashari <faride13578642@gmail.com>
2020-08-14 15:13:04 +0430

    add getOneUser event handler

--- src/webSocket/user/user.gateway.ts
+++ src/webSocket/user/user.gateway.ts
@@ -1,9 +1,21 @@
 import { SubscribeMessage, WebSocketGateway } from '@nestjs/websockets';
+import {Socket} from 'socket.io';
+import {} from 'src/graphql';
+import {UserService} from './user.service';
 
-@WebSocketGateway()
+@WebSocketGateway({namespace: "websocket"})
+
 export class UserGateway {
-  @SubscribeMessage('message')
-  handleMessage(client: any, payload: any): string {
-    return 'Hello world!';
-  }
+
+    private readonly userServices: UserService;
+
+    constructor(){
+        this.userServices = new UserService();
+    }
+
+    @SubscribeMessage('getOneUser')
+    async getOneUser(client: Socket, user_id: number){
+        const result = await this.userServices.getOneUser(user_id);
+        client.emit("getOneUser", result);
+    }
 }
